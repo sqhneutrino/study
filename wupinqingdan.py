ccc990df0debd38a63064b8231ae3cbfe8141afb
sqhneutrino <37101203+sqhneutrino@users.noreply.github.com>
2018-03-11 18:22:15 +0800

    Update wupinqingdan.py

--- wupinqingdan.py
+++ wupinqingdan.py
@@ -1,8 +1,6 @@
 qingdan={'apple':42,'pineapple':2,'rope':5,'dingzi':7,'firestick':8,'shuitong':4,'basin':9}
-def tongjdj(dict):
-    total=0
-    for k,v in dict.items():
-        print(k+'---------'+str(v))
-        total=total+dict.get(k,0)#可以用，total+=v 代替
-    print('总共'+str(total)+'个')
-tongjdj(qingdan)
+total=0
+for k,v in qingdan.items():
+    print((k+':').ljust(15)+'          '+(str(v)+'个').rjust(5))
+    total=total+qingdan.get(k,0)
+print(('总共'+':').ljust(15)+(str(total)+'个').rjust(13))
